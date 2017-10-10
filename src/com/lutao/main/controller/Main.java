e7312a65fecee210597be6ec55f5f6155f1f550d
wwl <1848607815@qq.com>
2017-10-10 12:06:03 +0800

    wwl

---
+++ src/com/lutao/main/controller/Main.java
@@ -0,0 +1,247 @@
+package com.lutao.main.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.lutao.america.vo.OrderFillParameter;
+
+@Controller
+@RequestMapping("/main")
+public class Main {
+	//=================================================================滚动
+	@RequestMapping("/gd1")
+	public String gd1(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/gd1";
+	}
+	@RequestMapping("/gd2")
+	public String gd2(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/gd2";
+	}
+	@RequestMapping("/gd3")
+	public String gd3(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/gd3";
+	}
+	@RequestMapping("/gd4")
+	public String gd4(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/gd4";
+	}
+	@RequestMapping("/gd5")
+	public String gd5(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/gd5";
+	}
+	//=====================================================================普通
+	@RequestMapping("/lq1")
+	public String lq1(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq1";
+	}
+	@RequestMapping("/lq2")
+	public String lq2(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq2";
+	}
+	@RequestMapping("/lq3")
+	public String lq3(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq3";
+	}
+	@RequestMapping("/lq4")
+	public String lq4(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq4";
+	}
+	@RequestMapping("/lq5")
+	public String lq5(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq5";
+	}
+	@RequestMapping("/lq6")
+	public String lq6(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq6";
+	}
+	@RequestMapping("/lq7")
+	public String lq7(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq7";
+	}
+	@RequestMapping("/lq8")
+	public String lq8(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq8";
+	}
+	@RequestMapping("/lq9")
+	public String lq9(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq9";
+	}
+	@RequestMapping("/lq10")
+	public String lq10(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq10";
+	}
+	@RequestMapping("/lq11")
+	public String lq11(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq11";
+	}
+	@RequestMapping("/lq12")
+	public String lq12(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq12";
+	}
+	@RequestMapping("/lq13")
+	public String lq13(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq13";
+	}
+	@RequestMapping("/lq14")
+	public String lq14(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq14";
+	}
+	@RequestMapping("/lq15")
+	public String lq15(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq15";
+	}
+	@RequestMapping("/lq16")
+	public String lq16(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq16";
+	}
+	@RequestMapping("/lq17")
+	public String lq17(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq17";
+	}
+	@RequestMapping("/lq18")
+	public String lq18(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq18";
+	}
+	@RequestMapping("/lq19")
+	public String lq19(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq19";
+	}
+	@RequestMapping("/lq20")
+	public String lq20(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq10";
+	}
+	@RequestMapping("/lq21")
+	public String lq21(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq21";
+	}
+	@RequestMapping("/lq22")
+	public String lq22(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq22";
+	}	
+	@RequestMapping("/lq23")
+	public String lq23(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq23";
+	}	
+	@RequestMapping("/lq24")
+	public String lq24(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq24";
+	}	
+	@RequestMapping("/lq25")
+	public String lq25(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq25";
+	}	
+	@RequestMapping("/lq26")
+	public String lq26(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/lq26";
+	}
+	//===========================================================================================抢购
+	@RequestMapping("/qg1")
+	public String qg1(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg1";
+	}
+	@RequestMapping("/qg2")
+	public String qg2(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg2";
+	}	
+	@RequestMapping("/qg3")
+	public String qg3(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg3";
+	}	
+	@RequestMapping("/qg4")
+	public String qg4(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg4";
+	}	
+	@RequestMapping("/qg5")
+	public String qg5(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg5";
+	}	
+	@RequestMapping("/qg6")
+	public String qg6(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg6";
+	}	
+	@RequestMapping("/qg7")
+	public String qg7(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg7";
+	}	
+	@RequestMapping("/qg8")
+	public String qg8(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg8";
+	}	
+	@RequestMapping("/qg9")
+	public String qg9(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg9";
+	}	
+	@RequestMapping("/qg10")
+	public String qg10(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg10";
+	}	
+	@RequestMapping("/qg11")
+	public String qg11(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg11";
+	}	
+	@RequestMapping("/qg12")
+	public String qg12(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg12";
+	}	
+	@RequestMapping("/qg13")
+	public String qg13(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg13";
+	}	
+	@RequestMapping("/qg14")
+	public String qg14(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg14";
+	}
+	@RequestMapping("/qg15")
+	public String qg15(OrderFillParameter params,HttpServletRequest req) {
+		System.out.println("进来");
+		return "view/qg15";
+	}
+	
+}
